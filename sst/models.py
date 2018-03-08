850b314508e881b6046e048245820ebec9d2c642
gescoba5 <gescobaruribe@gmail.com>
2018-03-08 15:17:58 -0500

    Cambio de HTML para la página

--- sst/models.py
+++ sst/models.py
@@ -5,9 +5,9 @@ from django.db import models
 from django.utils import timezone
 
 class Post(models.Model):
-  author = models.ForeignKey('auth.User', on_delete = models.CASCADE) # Clave foranea, vinculo a otro modelo
-  title = models.CharField(max_length=200)  # Define un texto con un número limitado de caracteres
-  text = models.TextField() # Define textos largos sin ĺimites
+  author       = models.ForeignKey('auth.User', on_delete = models.CASCADE) # Clave foranea, vinculo a otro modelo
+  title        = models.CharField(max_length = 200)  # Define un texto con un número limitado de caracteres
+  text         = models.TextField() # Define textos largos sin ĺimites
   created_date = models.DateTimeField(
     default = timezone.now)
   published_date = models.DateTimeField(
