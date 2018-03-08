34dd46ae979a15272bd864f0cb2b7f7599b6aa8c
gescoba5 <gescobaruribe@gmail.com>
2018-03-08 16:21:17 -0500

    Cambio settings y BD

--- pruebaApp/settings.py
+++ pruebaApp/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'pruebaApp.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'prueba_form.db',
+        'NAME': os.path.join(BASE_DIR, 'prueba_form.db'),
     }
 }
 
